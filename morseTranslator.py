d74790b8ba612e040ed85bdb64916514a70e777d
devynchew <devynchew@gmail.com>
2021-10-28 19:37:01 +0800

    Add stopwords.txt and morse translator py file

---
+++ morseTranslator.py
@@ -0,0 +1,64 @@
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----'}
+
+def encodeMorse(file):
+    f = open(file, "r")
+    text = f.read()
+    str = ''
+    i = 0
+    for letter in text:
+        if letter != ' ':
+            if letter.isalnum():
+                if i == len(text) - 1:
+                    str += MORSE_CODE_DICT[letter]
+                else:
+                    str += MORSE_CODE_DICT[letter] + ','
+            else:
+                str += letter
+        else:
+            str += ' ,'
+        i += 1
+    return str
+
+
+def decodeMorse(file):
+    f = open(file, "r")
+    text = f.read()
+    morsetext = ''
+    translated = ''
+    i = 0
+
+    for char in text:
+        if char != ',':
+            if char != ' ':
+                if char != '\n':
+                    morsetext += char
+                else: # char is a new line
+                    translated += list(MORSE_CODE_DICT.keys())[list(MORSE_CODE_DICT.values()).index(morsetext)] + '\n'
+                    morsetext = ''
+            else: # char is a space
+                translated += ' '
+        else: # char is a comma
+            if morsetext != '':
+                translated += list(MORSE_CODE_DICT.keys())[list(MORSE_CODE_DICT.values()).index(morsetext)]
+                morsetext = ''
+        i += 1
+    translated += list(MORSE_CODE_DICT.keys())[list(MORSE_CODE_DICT.values()).index(morsetext)]
+    return translated
+
+outputMorse = encodeMorse("eng.txt")
+print(outputMorse)
+
+outputEnglish = decodeMorse("morse.txt")
+print(outputEnglish)
